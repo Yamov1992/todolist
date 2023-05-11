b5d0b244bd3874ff62f87e682c6db83a62842912
ALFA <pvp624140@gmail.com>
2023-05-11 19:21:17 +0300

    lesson_37_boards

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from todolist.goals import views
 
 urlpatterns = [
+
+    path("board/create", views.BoardCreateView.as_view(), name='create-board'),
+    path("board/list", views.BoardListView.as_view(), name='board-list'),
+    path("board/<int:pk>", views.BoardDetailView.as_view(), name='board'),
+
     path("goal_category/create", views.GoalCategoryCreateView.as_view(), name='create-category'),
     path("goal_category/list", views.GoalCategoryListView.as_view(), name='category-list'),
     path("goal_category/<int:pk>", views.GoalCategoryView.as_view(), name='goal-category'),
@@ -14,4 +19,5 @@ urlpatterns = [
     path("goal_comment/create", views.GoalCommentCreateView.as_view(), name='create-comment'),
     path("goal_comment/list", views.GoalCommentListView.as_view(), name='comment-list'),
     path("goal_comment/<int:pk>", views.GoalCommentView.as_view(), name='comment'),
+
 ]
--- todolist/goals/views.py
+++ todolist/goals/views.py
@@ -1,16 +1,50 @@
 from django.db import transaction
 from django.db.models import QuerySet
 from django_filters.rest_framework import DjangoFilterBackend
-from todolist.permissions import GoalCommentPermission
+from todolist.permissions import GoalCommentPermission, BoardPermission, GoalPermission, GoalCategoryPermission
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import LimitOffsetPagination
 
 from todolist.goals.filters import GoalDateFilter
-from todolist.goals.models import GoalCategory, Goal, GoalComment
+from todolist.goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
 from todolist.goals.serializers import GoalCategoryCreateSerializer, GoalCategorySerializer, GoalCreateSerializer, \
-    GoalSerializer, GoalCommentWithUserSerializer, GoalCommentSerializer
+    GoalSerializer, GoalCommentWithUserSerializer, GoalCommentSerializer, BoardSerializer, BoardParticipantSerializer
+
+
+class BoardCreateView(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BoardSerializer
+
+    def perform_create(self, serializer: BoardSerializer) -> None:
+        """Делаем текущего пользователя владельцем доски"""
+        BoardParticipant.objects.create(user=self.request.user, board=serializer.save())
+
+
+class BoardListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BoardSerializer
+    filter_backends = [OrderingFilter]
+    ordering = ['title']
+
+
+    def get_queryset(self) -> QuerySet[Board]:
+        return Board.objects.filter(participants__user_id=self.request.user.id).exclude(is_deleted=True)
+
+
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [BoardPermission]
+    serializer_class = BoardParticipantSerializer
+
+    def get_queryset(self) -> QuerySet[Board]:
+        return Board.objects.filter(participants__user_id=self.request.user.id).exclude(is_deleted=True)
+
+    def perform_destroy(self, instance: Board) -> None:
+        with transaction.atomic():
+            Board.objects.filter(id=instance.id).update(is_deleted=True)
+            instance.categories.update(is_deleted=True)
+            Goal.objects.filter(category__board=instance).update(status=Goal.Status.archived)
 
 
 class GoalCategoryCreateView(generics.CreateAPIView):
@@ -22,17 +56,19 @@ class GoalCategoryListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalCategorySerializer
     filter_backends = [OrderingFilter, SearchFilter]
+    # filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['goal']
     ordering_fields = ('title', 'created')
     ordering = ['title']
     search_fields = ['title']
     pagination_class = LimitOffsetPagination
 
-    def get_queryset(self):
-        return GoalCategory.objects.select_related('user').filter(user=self.request.user, is_deleted=False)
+    def get_queryset(self) -> QuerySet[GoalCategory]:
+        return GoalCategory.objects.filter(board__participants__user=self.request.user).exclude(is_deleted=True)
 
 
 class GoalCategoryView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [GoalCategoryPermission]
     serializer_class = GoalCategorySerializer
 
     def get_queryset(self):
@@ -53,6 +89,7 @@ class GoalCreateView(generics.CreateAPIView):
 class GoalListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalSerializer
+    # filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filterset_class = GoalDateFilter
     ordering_fields = ('title', 'created')
@@ -66,7 +103,7 @@ class GoalListView(generics.ListAPIView):
 
 
 class GoalView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [GoalPermission]
     serializer_class = GoalSerializer
 
     def get_queryset(self):
@@ -88,7 +125,8 @@ class GoalCommentCreateView(generics.CreateAPIView):
 class GoalCommentListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalCommentWithUserSerializer
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filter_backends = [OrderingFilter]
+    # filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['goal']
     ordering = ['-created']
 
@@ -97,6 +135,6 @@ class GoalCommentListView(generics.ListAPIView):
 
 
 class GoalCommentView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [GoalCommentPermission]
     serializer_class = GoalCommentWithUserSerializer
     queryset = GoalComment.objects.select_related('user')
